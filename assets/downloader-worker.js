9cd496f181629820f35b9e7309f08fed3df40de6
Owen <owen841213@gmail.com>
2017-12-09 12:41:12 +0800

    Updates

--- assets/downloader-worker.js
+++ assets/downloader-worker.js
@@ -1,3 +1,3 @@
-/*! wordcloud-app  2017-12-08 */
+/*! wordcloud-app  2017-12-09 */
 
 "use strict";self.callback=function(){var a=Array.prototype.slice.call(arguments);self.postMessage(a)},self.onmessage=function(a){var b=a.data;b+=-1===b.indexOf("?")?"?":"&",b+="callback=callback",self.importScripts(b)};
